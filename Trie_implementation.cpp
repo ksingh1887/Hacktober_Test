e324d5e3eac3f7412266b46655e7797603a8ccf4
ksingh1887 <68816239+ksingh1887@users.noreply.github.com>
2022-10-15 20:11:07 +0530

    Create Trie_implementation.cpp

---
+++ Trie_implementation.cpp
@@ -0,0 +1,26 @@
+// C++ implementation of search and insert
+// operations on Trie
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+// trie node
+struct TrieNode
+{
+	struct TrieNode *children[ALPHABET_SIZE];
+
+	// isEndOfWord is true if the node represents
+	// end of a word
+	bool isEndOfWord;
+};
+
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void)
+{
+	struct TrieNode *pNode = new TrieNode;
+
+	pNode->isEndOfWord = false;
+
+	for (int i = 0; i < ALPHABET_SIZE; i++)
